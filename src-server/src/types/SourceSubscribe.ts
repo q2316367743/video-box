0d964ca880677543b661279b7fdd2e97909669c1
落雨不悔 <m17762618644@163.com>
2025-08-08 18:59:04 +0800

    feat(types): 扩展 SourceSubscribe 类型定义
    - 新增 RSS Hub 订阅类型
    - 添加自定义订阅规则接口
    - 新增订阅源记录和内容接口
    - 更新订阅类型枚举值

--- src-server/src/types/SourceSubscribe.ts
+++ src-server/src/types/SourceSubscribe.ts
@@ -1,12 +1,13 @@
 import {YesOrNoType} from "@/global/constant";
 
-// 订阅类型：RSS-1，内部订阅-2，自定义订阅-3
+// 订阅类型：RSS-1, rss hub-2，内部订阅-3，自定义订阅-4
 export type SourceSubscribeType = 1 | 2 | 3;
 // 展示方式：1-文章，2-社交媒体，3-图片，4-视频，5-音频，6-通知
 export type SourceSubscribeDisplay = 1 | 2 | 3 | 4 | 5 | 6;
 
 /**
  * 订阅源
+ * @see https://docs.rsshub.app/zh/
  */
 export interface SourceSubscribe {
   id: string;
@@ -24,4 +25,105 @@ export interface SourceSubscribe {
   updated_at: number;
   // 是否缓存，不缓存每次都是最新的列表
   cache: YesOrNoType;
+}
+
+/**
+ * 订阅规则
+ * @see https://app.follow.is/discover?type=transform
+ */
+export interface SourceSubscribeRule {
+  id: string;
+  subscribe_id: string;
+
+  // ======================= 内部订阅 =======================
+
+  data: string;
+
+  // ======================= 自定义订阅 =======================
+
+  // 列表规则
+  list: string;
+  item: string;
+  // 标题规则
+  item_title: string;
+  // 描述规则
+  item_description: string;
+  // 最后更新时间
+  item_pub_date: string;
+  // 连接规则
+  item_link: string;
+  // 媒体规则
+  item_media: string;
+
+  // 内容规则
+  item_content: string;
+  // 编码规则
+  item_charset: string;
+
+}
+
+/**
+ * 订阅源 RSS Hub 实例
+ */
+export interface SourceSubscribeRssHub {
+  id: string;
+  created_at: number;
+  updated_at: number;
+  name: string;
+  description: string;
+  url: string;
+  // 刷新时间
+  refresh_time: number;
+  // 重试次数
+  retry_count: number;
+  // 延迟时间
+  delay_time: number;
+  // 是否启用
+  is_enabled: number;
+}
+
+/**
+ * 订阅浏览记录
+ */
+export interface SourceSubscribeRecord {
+  id: string;
+  created_at: number;
+  updated_at: number;
+
+  // 所属订阅源
+  subscribe_id: string;
+  // 签名，唯一
+  sign: string;
+
+  title: string;
+  description: string;
+  // 最后更新时间
+  pub_date: string;
+  media: string;
+  link: string;
+}
+
+/**
+ * 订阅内容
+ */
+export interface SourceSubscribeContent {
+  id: string;
+  created_at: number;
+  updated_at: number;
+
+  // 所属订阅源
+  subscribe_id: string;
+  // 原文链接
+  link: string;
+
+  /**
+   * 实际内容
+   */
+  content: string;
+
+  /**
+   * AI总结
+   */
+  ai: string;
+
 }
